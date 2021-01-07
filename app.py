c9f7940f06d1db306999b27fb0361285c27fa7cf
Corbin Beebe <corbinbeebe@gmail.com>
2021-01-06 17:33:17 -0800

    added checkbox functionality and migrations

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, abort, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -8,31 +9,60 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # db object links SQLAlchemy to flask application
 db = SQLAlchemy(app)
 
+# instantiate migrate to allow for use of flask-migrate
+migrate = Migrate(app, db)
+
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 
-# creates tables for all models created
-db.create_all()
-
-
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    # Try to commit the new record to the db
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description, completed=False)
+        db.session.add(todo)
+        db.session.commit()
+        body['completed'] = todo.completed
+        body['description'] = todo.description
+    # if the commit fails, roll back the session
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+@app.route('/', methods=['Post', 'GET'])
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', todos=Todo.query.order_by('id').all())
